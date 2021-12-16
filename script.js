c200471b2f3b1a938d8fff192ac94805b19bc707
yogesh973 <42474630+yogesh973@users.noreply.github.com>
2021-12-16 10:11:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateFood, draw as drawFood } from './food.js'
+import { outsideGrid } from './grid.js'
+import {expandSnake} from './snake.js'
+
+
+
+let lastRenderTime = 0
+let gameOver = false
+const gameBoard = document.getElementById('game-board')
+const points=document.getElementById('points')
+
+var myModal = new bootstrap.Modal(document.getElementById("exampleModalCenter"), {});
+
+
+
+function main(currentTime) {
+  if (gameOver) {
+   
+      expandSnake()
+      myModal.show();
+     
+     
+    // if (confirm('You lost. Press ok to restart.')) {
+    //   window.location = '/'
+    // }
+    return
+  }
+
+
+  window.requestAnimationFrame(main)
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+
+  lastRenderTime = currentTime
+
+  update()
+  draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+  updateSnake()
+  updateFood()
+  checkDeath()
+}
+
+function draw() {
+  gameBoard.innerHTML = ''
+  drawSnake(gameBoard)
+  drawFood(gameBoard)
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
+
+
+hello()
+
+function hello(){
+   
+}
